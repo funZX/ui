cd0f19151a3aa63c1a507e32523dbf3098da95bf
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-23 22:30:38 +0200

    Revert "cleanup"
    This reverts commit c9f3f9f609a51bb1269073a80e4226e2c8668945.

--- lua/nvchad/blink/lazyspec.lua
+++ lua/nvchad/blink/lazyspec.lua
@@ -1,10 +1,31 @@
+-- BETA
+--  TO use this put this in your plugins table
+-- { import = "nvchad.blink.lazyspec" },
+
 return {
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+  },
+
   {
     "saghen/blink.cmp",
     version = "1.*",
     event = { "InsertEnter", "CmdLineEnter" },
 
     dependencies = {
+      "rafamadriz/friendly-snippets",
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+          require "nvchad.configs.luasnip"
+        end,
+      },
+
       {
         "windwp/nvim-autopairs",
         opts = {
