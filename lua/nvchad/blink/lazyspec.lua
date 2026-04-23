c9f3f9f609a51bb1269073a80e4226e2c8668945
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-23 22:04:00 +0200

    cleanup

--- lua/nvchad/blink/lazyspec.lua
+++ lua/nvchad/blink/lazyspec.lua
@@ -1,31 +1,10 @@
--- BETA
---  TO use this put this in your plugins table
--- { import = "nvchad.blink.lazyspec" },
-
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    enabled = false,
-  },
-
   {
     "saghen/blink.cmp",
     version = "1.*",
     event = { "InsertEnter", "CmdLineEnter" },
 
     dependencies = {
-      "rafamadriz/friendly-snippets",
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require "nvchad.configs.luasnip"
-        end,
-      },
-
       {
         "windwp/nvim-autopairs",
         opts = {
