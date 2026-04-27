6a398391009ea1e8b704ba5e62d9867d04e8c0e8
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-27 12:53:27 +0200

    -nvchad.term

--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -40,21 +40,6 @@ local options = {
     },
   },
 
-  term = {
-    startinsert = true,
-    base46_colors = true,
-    winopts = { number = false, relativenumber = false },
-    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-    float = {
-      relative = "editor",
-      row = 0.3,
-      col = 0.25,
-      width = 0.5,
-      height = 0.4,
-      border = "single",
-    },
-  },
-
   lsp = { signature = true },
 
   colorify = {
